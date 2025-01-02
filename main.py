f0d3e29ba4f2f7344c74833f84d6d362499a0261
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2025-01-01 20:07:44 -0500

    add recipes to in-memory vector store and send relevant recipes to llm

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
+from langchain_community.document_loaders import CSVLoader
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
 
 
-
+# load environment variables
 env_found = load_dotenv(override=True, verbose=True)
-llm = ChatOpenAI(model="gpt-4o-mini")
-
 
 def get_response(input):
-    prompt = PromptTemplate.from_template("""Given the following user question, answer the user question.  
-    If you cannot find results, just say 'No results were found' 
+
+    # Parse CSV file with recipes into documents
+    file = "./data/sample_recipes.csv"
+    loader = CSVLoader(file_path=file)
+    documents = loader.load()
+
+    # Initialize embedding model
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+    # Create an in-memory vector store using embedding model
+    vector_store = InMemoryVectorStore(embedding=embedding_model)
+
+    # Add recipes to the vector store
+    vector_store.add_documents(documents)
+
+    # Search for meals based on user input
+    retriever = vector_store.as_retriever()
+    relevant_recipes = retriever.invoke(input)
+
+    # Set up LLM
+    llm = ChatOpenAI(model="gpt-4o-mini")
+
+    # Create a prompt template
+    template = ChatPromptTemplate([
+        ("system", f"""
+         
+         You are a a helpful meal planner. 
+         
+         Your job is to suggest meals based on the recipes and user input. 
+
+         If the user input is a question or request not related to generating a list of meals, reply 'Sorry I can't answer. I can only provide meal suggestions.'
+        
+         Recipes: {{relevant_recipes}}
+         """),
+         ("human", "{user_input}")
+    ])
     
-    Input: {input}
-    Answer: """)
+    # Generate prompt using user input and relevant recipes
+    prompt = template.invoke(
+        {
+        "relevant_recipes": relevant_recipes,
+        "user_input": input
+        })
 
-    chain = (prompt | llm | StrOutputParser())
+    # Get response from LLM
+    response = llm.invoke(prompt)
 
-    return chain.invoke({"input": input})
+    return response.content
+  
 
-
-print("I'm the meal planner chatbot. How can I help?")
+print("I'm the meal planner chatbot. How can I help you today?")
 
 while True:
     user_input = input ("You: ")
     if user_input.lower() == "exit":
         break
     response = get_response(user_input)
-    print(response)
+    print(f"AI: {response}")
 
 
